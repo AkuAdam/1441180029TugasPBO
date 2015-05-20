0b8a18ecfeabfbe149342d901a9dbcce2655f333
Fakhrial Adam <adam_dangdingdong@yahoo.co.id>
2015-05-20 23:11:13 +0700

    Revert "Revert "Tugas 5""
    This reverts commit 8f21079074f7833c1ce54d7183a9c48e155c825d.

---
+++ Tugas 5/Testmahasiswa.java
@@ -0,0 +1,12 @@
+public class Testmahasiswa {
+	public static void main(String[] args) {
+		mahasiswa mhs = new mahasiswa("Fakhrial Adam", 11);
+
+		mhs.tampilkanBiodata();
+		System.out.print("");
+		mhs.setUmur(19);
+		mhs.tampilkanBiodata();
+
+	}
+}
+	
